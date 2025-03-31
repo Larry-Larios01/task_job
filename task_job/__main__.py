684b6b2cdb12d949fe231585c7e1f2deb6f31e5d
larry123 <larioslarry8@gmail.com>
2025-03-31 14:51:56 -0600

    adding the funvtionality to get 1 million dates and filter the duplicated data

--- task_job/__main__.py
+++ task_job/__main__.py
@@ -1,1 +1,18 @@
-print("hello")
+import pandas as pd
+
+def one_million_data():
+    csv_file = pd.read_csv("resources/task.csv", sep="\t")
+    one_million_data = csv_file.head(1000000)
+    return one_million_data
+
+
+def duplicated_data()-> list[int]:
+    data = one_million_data()
+    duplicated = data[data.duplicated(subset=["<DATE>", "<TIME>"], keep=False)]
+
+    return duplicated
+
+if __name__ == "__main__":
+    # Código a ejecutar solo si este archivo se ejecuta directamente
+    csv_file = duplicated_data()
+    print(csv_file)
