735079b3b429b268c04069103565a687a1f0d337
larry123 <larioslarry8@gmail.com>
2025-03-31 22:09:45 -0600

    starting the creation of the plot

--- task_job/__main__.py
+++ task_job/__main__.py
@@ -1,44 +1,56 @@
 import pandas as pd
 import numpy as np
+import mplfinance as mf
 
-def one_million_data():
-    csv_file = pd.read_csv("resources/task.csv", sep="\t", nrows=1000000)
-    #one_million_data = csv_file.head(1000000)
-    return csv_file
+def load_data(path: str, sep: str = "\t", nrows: int = 1000000) -> pd.DataFrame:
+    return pd.read_csv(path, sep=sep, nrows=nrows)
 
-
-def duplicated_data()-> list[int]:
-    data = one_million_data()
+def get_duplicated_indices(data: pd.DataFrame) -> list[int]:
     duplicated = data[data.duplicated(subset=["<DATE>", "<TIME>"], keep=False)]
-    duplicated_with_Nan = duplicated[(duplicated['<BID>'].isna()) | (duplicated['<ASK>'].isna())]
-    list_of_duplicated_of_Nan = duplicated_with_Nan.index.to_list()
+    duplicated_with_nan = duplicated[
+        duplicated["<BID>"].isna() | duplicated["<ASK>"].isna()
+    ]
+    return duplicated_with_nan.index.to_list()
 
-    return list_of_duplicated_of_Nan
+def remove_duplicated_rows(data: pd.DataFrame, indices: list[int]) -> pd.DataFrame:
+    return data.drop(index=indices)
 
-def delete_duplicated_data():
-    data = one_million_data()
-    list_data_to_delete = duplicated_data()
-    leaked_data = data.drop(index=list_data_to_delete)
-    return leaked_data
+def add_spread_column(data: pd.DataFrame) -> pd.DataFrame:
+    data["<ASK>"] = data["<ASK>"].fillna(0)
+    data["<BID>"] = data["<BID>"].fillna(0)
+    data["<SPREAD>"] = data["<ASK>"] - data["<BID>"]
+    return data
 
-def create_new_column():
-    leaked_data = delete_duplicated_data()
-    leaked_data["<ASK>"] = leaked_data["<ASK>"].fillna(0)
-    leaked_data["<BID>"] = leaked_data["<BID>"].fillna(0)
-    leaked_data["<SPREAD>"] = leaked_data["<ASK>"] - leaked_data["<BID>"]
-    
-    return leaked_data
+def fill_missing_ask_bid(data: pd.DataFrame) -> pd.DataFrame:
+    data["<BID>"] = np.where(
+        data["<BID>"] == 0,
+        data["<ASK>"] - data["<SPREAD>"].shift(1),
+        data["<BID>"]
+    )
+    data["<ASK>"] = np.where(
+        data["<ASK>"] == 0,
+        data["<BID>"] + data["<SPREAD>"].shift(1),
+        data["<ASK>"]
+    )
+    return data
 
-def fill_ask_and_bid():
-    leaked_data = create_new_column()
-    leaked_data["<BID>"] = np.where(leaked_data["<BID>"] == 0, leaked_data["<ASK>"] - leaked_data["<SPREAD>"].shift(1), leaked_data["<BID>"])
-    leaked_data["<ASK>"] = np.where(leaked_data["<ASK>"] == 0, leaked_data["<BID>"] + leaked_data["<SPREAD>"].shift(1), leaked_data["<ASK>"])
-    return leaked_data   
+def create_candlestick_plot(data: pd.DataFrame):
+    ohlc = data["<BID>"].resample("1min").ohlc() 
+    mf.plot(ohlc, type='candle', style='charles', title="Gráfico de velas - BID", ylabel="Precio BID")
 
-def save_csv():
-    data = fill_ask_and_bid()
-    data.to_csv("task.csv", index=False)
+def save_data(data: pd.DataFrame, output_path: str):
+    data.to_csv(output_path, index=False)
+
+
+def main(input_path: str, output_path: str):
+    raw_data = load_data(input_path)
+    duplicate_indices = get_duplicated_indices(raw_data)
+    clean_data = remove_duplicated_rows(raw_data, duplicate_indices)
+    with_spread = add_spread_column(clean_data)
+    completed_data = fill_missing_ask_bid(with_spread)
+    #create_candlestick_plot(completed_data)
+    print(completed_data)
+    save_data(completed_data, output_path)
 
 if __name__ == "__main__":
-   data = save_csv()
-   
+    main("resources/task.csv", "task.csv")
